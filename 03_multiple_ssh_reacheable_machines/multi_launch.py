bc537152b93b27229db030c589fdc3477d145bd4
Felix <felix@31-35-189.wireless.csail.mit.edu>
2021-05-07 07:31:27 -0400

    debugging single_launch works

--- 03_multiple_ssh_reacheable_machines/multi_launch.py
+++ 03_multiple_ssh_reacheable_machines/multi_launch.py
@@ -4,13 +4,13 @@ from launch_entry import train_fn
 
 if __name__ == "__main__":
     hosts = [
-        'visiongpu001',
-        'visiongpu002',
-        'visiongpu003',
+        'vision03',
+        'vision04',
+        # 'visiongpu003',
     ]
 
     for host in hosts:
         jaynes.config(verbose=False, launch=dict(ip=host))
         jaynes.run(train_fn)
 
-    jaynes.listen(200)
+    # jaynes.listen(200)
